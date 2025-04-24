bd5e46facd4951895444795be964e4831c242195
Jackie Muzakki <jackie.muzakki@gmail.com>
2025-04-25 05:44:49 +0700

    chore(entrypoint): enhance logging and directory listing for debugging

--- example/simple/entrypoint.sh
+++ example/simple/entrypoint.sh
@@ -5,8 +5,13 @@
 gripmock --stub=example/simple/stub example/simple/simple.proto &
 
 cat go.mod
-
-ls protogen/example/simple
+echo "================="
+cat /go/src/grpc/server.go
+echo "================="
+cat /go/src/grpc/go.mod
+echo "================="
+ls -R /go/src/
+echo "================="
 
 # wait for generated files to be available and gripmock is up
 sleep 20
